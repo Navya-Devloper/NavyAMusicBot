911f9b594f55f46fefd3b7c2a14505cad84af8f0
Navya Devloper <100741130+Navya-Devloper@users.noreply.github.com>
2022-04-02 09:55:29 +0530

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -37,9 +37,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001617464043
+                                and chat_id != -1001617464043
+                                and chat_id != -1001617464043
                             ):
                                 if not await is_active_chat(chat_id):
                                     try:
